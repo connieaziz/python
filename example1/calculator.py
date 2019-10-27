30b5fb4bbbc577983f8dfefa0ed59989595c5792
Consolata Akoth <47849117+connieaziz@users.noreply.github.com>
2019-10-27 21:40:53 +0300

    Add files via upload

---
+++ example1/calculator.py
@@ -0,0 +1,23 @@
+def add(a,b):
+	answer=a+b
+	return answer
+
+def subtract(a,b):
+	answer=a-b
+	return answer
+
+def multiply(a,b):
+	answer=a*b
+	return answer
+
+def modules(a,b):
+	answer=a%b
+	return answer
+
+def divide(a,b):
+	answer=a/b
+	return answer
+
+def exponent(a,b):
+	answer=a**b
+	return answer
